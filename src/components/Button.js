ad060e34334d2463bcf0c36d6400aad0d533d033
kishu1998 <kishorsanjaypatil1998@gmail.com>
2019-08-06 17:14:30 +0530

    hosted

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+
+export default {
+  
+button : {
+textTransform:'capitalize',
+fontSize:'1.4rem',
+background:'transparent',
+border:'0.05rem solid var(--lightBlue)',
+borderRadius:'0.5rem',
+padding:'0.2rem 0.5rem',
+cursor:'pointer',
+margin:'0.2rem 0.5rem 0.2rem 0',
+transition:'all 05s ease-in-out'
+}
+
+}
