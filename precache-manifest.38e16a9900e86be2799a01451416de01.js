74c793fff44ec868aa885cd26d45e6e0d288034a
kishu1998 <kishorsanjaypatil1998@gmail.com>
2019-08-06 17:15:28 +0530

    Updates

---
+++ precache-manifest.38e16a9900e86be2799a01451416de01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7bcc6cdff13efaa34735918a7b0686f",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "cdb170fac9720cb28123",
+    "url": "/store/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "978583be609792812120",
+    "url": "/store/static/css/main.a82102ab.chunk.css"
+  },
+  {
+    "revision": "cdb170fac9720cb28123",
+    "url": "/store/static/js/2.4dcae750.chunk.js"
+  },
+  {
+    "revision": "978583be609792812120",
+    "url": "/store/static/js/main.256ab896.chunk.js"
+  },
+  {
+    "revision": "4642218ef387c551e27b",
+    "url": "/store/static/js/runtime~main.852bf9be.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/store/static/media/logo.9f7e506d.svg"
+  }
+]);
